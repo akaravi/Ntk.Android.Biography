0476e14f2b054637d23bf25bd2375b6a5bf1e0aa
me <zsdg98@gmail.com>
2019-06-26 09:37:19 +0430

    Fix Bugs.

--- app/src/main/java/ntk/android/biography/config/ConfigRestHeader.java
+++ app/src/main/java/ntk/android/biography/config/ConfigRestHeader.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.provider.Settings;
 import android.telephony.TelephonyManager;
+import android.util.Base64;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 
@@ -24,8 +25,8 @@ public class ConfigRestHeader {
         headers.put("LocationLat", "0");
         headers.put("DeviceId", Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID));
         headers.put("DeviceBrand", AppUtill.GetDeviceName());
-        headers.put("Country", context.getResources().getConfiguration().locale.getDisplayCountry());
-        headers.put("Language", context.getResources().getConfiguration().locale.getLanguage());
+        headers.put("Country", Base64.encodeToString(context.getResources().getConfiguration().locale.getDisplayCountry().getBytes(), Base64.NO_WRAP));
+        headers.put("Language", Base64.encodeToString(context.getResources().getConfiguration().locale.getLanguage().getBytes(), Base64.NO_WRAP));
         headers.put("SimCard", manager.getSimOperatorName());
         headers.put("PackageName", "ntk.cms.vitrin.app11");
         headers.put("AppBuildVer", String.valueOf(BuildConfig.VERSION_CODE));
